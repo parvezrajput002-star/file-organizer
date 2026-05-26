d10cc5c996d78511be80aec00be526ca5d93b049
parvez <parvezrajput002@gmail.com>
2026-05-26 22:02:53 +0530

    organiser upgrade with the file counter !

--- file-organiser.py
+++ file-organiser.py
@@ -2,9 +2,15 @@ import os
 import shutil as sh
 import time 
 
+# system folder files path 
 path = r'C:\Users\parve\Downloads'
 files_path = os.listdir(path)
+
+# define the destination system folder 
+
 D_path = os.listdir("D:")
+
+#file dictionary to compare with the system files 
 
 file_name_dictionary = {
     "PDF" : [".pdf"],
@@ -18,21 +24,31 @@ file_name_dictionary = {
     "Software / Installer" : [".exe", ".msi", ".apk"],
     "Chrome Temporary Download" : [".crdownload"]
 }
+
+count = 0
+
+# loop : split the file and extension into two parts 
 for file in files_path:
     primary_name , extension = os.path.splitext(file)
     Extension = extension.lower()
+    # store the folder name and extension list from the dictionary
     for category , ext_list in file_name_dictionary.items():
+        # iterate all the extension from the extension list 
         for ext in ext_list:
+            # compare system file folder extension with the dictionary file extension 
             if Extension == ext:
                 print("Extension Matched !")
+                # after match , add system folder path with the matched file that creates new system folder path 
                 src = os.path.join(path,file)
+                # searching folder in D disk to move file 
                 for folder in D_path:
                     if folder == category :
                         destination = os.path.join("D:\\" , category,file)
-                        checking = os.path.exists(destination)
                         time.sleep(3)
                         print("Files Moved in few Seconds !")
                         moving = sh.move(src,destination)
                         print("your files are moved from another folder !")
+                count = count+1
+                print(f"file moved : {count}")
                         
 print("organizer have ran successfully !")         
